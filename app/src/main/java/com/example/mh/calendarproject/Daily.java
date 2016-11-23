d1f0565db2d409013d9fc9d69663d4ae11e5a261
cha_93 <minhye_love@naver.com>
2016-11-23 21:06:31 +0900

    fragment

---
+++ app/src/main/java/com/example/mh/calendarproject/Daily.java
@@ -0,0 +1,23 @@
+package com.example.mh.calendarproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sea on 2016-10-12.
+ */
+
+public class Daily extends Fragment {
+
+    public Daily(){}
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_daily,container,false);
+    }
+}
--- app/src/main/java/com/example/mh/calendarproject/MainActivity.java
+++ app/src/main/java/com/example/mh/calendarproject/MainActivity.java
@@ -1,37 +1,25 @@
 package com.example.mh.calendarproject;
 
 import android.app.ActionBar;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.CalendarView;
-import android.widget.GridView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Monthly FA = new Monthly();
+    final Weekly FB = new Weekly();
+    final Daily FC = new Daily();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -41,21 +29,39 @@ public class MainActivity extends AppCompatActivity {
                 actionBar.setHomeAsUpIndicator(drawable);
             }
         }
-        CalendarView calendar=(CalendarView)findViewById(R.id.calendar);
+/*        CalendarView calendar=(CalendarView)findViewById(R.id.calendar);
 
         calendar.setOnDateChangeListener((new CalendarView.OnDateChangeListener(){
             public void onSelectedDayChange(CalendarView view, int year,int month, int dayOfMonth){
                 //Log.i("View.getDate", view.getDate() + "");
-                Calendar calendar =Calendar.getInstance();
-                calendar.set(year,month,dayOfMonth);
-                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+                //Calendar calendar =Calendar.getInstance();
+               // calendar.set(year,month,dayOfMonth);
+                //int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
             }
-        }));
+        }));*/
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.fa:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main, FA).addToBackStack(null).commit();
+                Log.i("dd","213");
+                return true;
+            case R.id.fb:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main, FB).addToBackStack(null).commit();
+                return true;
+            case R.id.fc:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main, FC).addToBackStack(null).commit();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
